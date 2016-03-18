bc846df05957ace43f993b5e7e4ce6ce90af43a0
Christian Amaral <darthcas@gmail.com>
2016-03-18 15:43:19 -0300

    external account migrate 101

--- migrations/20160318141935_externalAccounts.js
+++ migrations/20160318141935_externalAccounts.js
@@ -1,13 +1,51 @@
 exports.up = function (knex, Promise) {
   return knex.schema
-    .createTable('platform')
-    .createTable('company_account')
-    .createTable('company_account_adwords')
+    .createTable('platform', function (table) {
+      table.string('id', 20).primary()
+      table.string('name', 30).notNullable()
+      table.timestamp('creation').notNullable().defaultTo(knex.fn.now())
+    })
+    .createTable('account', function (table) {
+      table.uuid('id').primary()
+
+      table.string('platform', 20)
+        .references('id')
+        .inTable('platform')
+        .onDelete('restrict')
+        .onUpdate('restrict')
+        .notNullable()
+
+
+      table.string('external_id', 40).notNullable()
+
+      table.timestamp('creation').notNullable().defaultTo(knex.fn.now())
+    })
+    .createTable('company_account', function (table) {
+      table.uuid('id').primary()
+
+      table.uuid('account')
+        .references('id')
+        .inTable('account')
+        .onDelete('cascade')
+        .onUpdate('restrict')
+        .notNullable()
+
+      table.uuid('company')
+        .references('id')
+        .inTable('company')
+        .onDelete('cascade')
+        .onUpdate('restrict')
+        .notNullable()
+
+      table.unique(['account', 'company'])
+
+      table.timestamp('creation').notNullable().defaultTo(knex.fn.now())
+    })
 }
 
 exports.down = function (knex, Promise) {
   return knex.schema
     .dropTable('platform')
+    .dropTable('account')
     .dropTable('company_account')
-    .dropTable('company_account_adwords')
 }
