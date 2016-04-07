d6bd51a71a0557953cb11dc40e0740b36a39da39
Christian Amaral <darthcas@gmail.com>
2016-04-07 17:31:10 -0300

    apps base schema

---
+++ migrations/20160407154647_apps.js
@@ -0,0 +1,59 @@
+exports.up = function (knex, Promise) {
+  return knex.schema
+    .createTable('app', function (table) {
+      table.string('id', 20).primary()
+      table.string('name', 40).notNullable()
+      table.timestamp('creation')
+        .notNullable()
+        .defaultTo(knex.fn.now())
+    })
+    .createTable('company_app', function (table) {
+      table.uuid('id').primary()
+
+      table.string('app', 20)
+        .references('id')
+        .inTable('app')
+        .onDelete('cascade')
+        .onUpdate('restrict')
+        .notNullable()
+
+      table.uuid('company')
+        .references('id')
+        .inTable('company')
+        .onDelete('restrict')
+        .onUpdate('restrict')
+        .notNullable()
+
+      table.timestamp('creation')
+        .notNullable()
+        .defaultTo(knex.fn.now())
+    })
+    .createTable('company_permission', function (table) {
+      table.uuid('id').primary()
+
+      table.uuid('company')
+        .references('id')
+        .inTable('company')
+        .onDelete('restrict')
+        .onUpdate('restrict')
+        .notNullable()
+
+      table.string('permission', 20)
+        .references('id')
+        .inTable('permission')
+        .onDelete('cascade')
+        .onUpdate('restrict')
+        .notNullable()
+
+      table.timestamp('creation')
+        .notNullable()
+        .defaultTo(knex.fn.now())
+    })
+}
+
+exports.down = function (knex, Promise) {
+  return knex.schema
+    .dropTable('app')
+    .dropTable('company_app')
+    .dropTable('company_permission')
+}
