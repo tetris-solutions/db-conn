82368806a039291929bd97b658ecf5581b355d74
Christian Amaral <darthcas@gmail.com>
2016-04-08 16:18:46 -0300

    creates the concept of plans

---
+++ migrations/20160408160037_plans.js
@@ -0,0 +1,60 @@
+exports.up = function (knex, Promise) {
+  return knex.schema
+    .dropTable('company_app')
+    .createTable('plan', function (table) {
+      table.uuid('id').primary()
+      table.timestamp('creation')
+        .notNullable()
+        .defaultTo(knex.fn.now())
+    })
+    .createTable('plan_name', function (table) {
+      table.uuid('id').primary()
+      table.string('name', 40).notNullable()
+
+      table.uuid('plan')
+        .references('id')
+        .inTable('plan')
+        .onDelete('cascade')
+        .onUpdate('restrict')
+        .notNullable()
+
+      table.string('locale', 5)
+        .references('id')
+        .inTable('locale')
+        .onDelete('cascade')
+        .onUpdate('restrict')
+        .notNullable()
+
+      table.timestamp('creation')
+        .notNullable()
+        .defaultTo(knex.fn.now())
+    })
+    .createTable('plan_permission', function (table) {
+      table.uuid('id').primary()
+
+      table.uuid('plan')
+        .references('id')
+        .inTable('plan')
+        .onDelete('cascade')
+        .onUpdate('restrict')
+        .notNullable()
+
+      table.string('permission', 20)
+        .references('id')
+        .inTable('permission')
+        .onDelete('cascade')
+        .onUpdate('restrict')
+        .notNullable()
+
+      table.timestamp('creation')
+        .notNullable()
+        .defaultTo(knex.fn.now())
+    })
+}
+
+exports.down = function (knex, Promise) {
+  return knex.schema
+    .dropTable('plan_permission')
+    .dropTable('plan_name')
+    .dropTable('plan')
+}
