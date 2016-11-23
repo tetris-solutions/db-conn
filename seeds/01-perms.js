c1fa5ee7e3a71d5669800ec19578020436fd9b6e
Christian Amaral <darthcas@gmail.com>
2016-11-23 11:20:11 -0200

    redefine global permissions

--- seeds/01-perms.js
+++ seeds/01-perms.js
@@ -3,8 +3,9 @@ var predefinedRoles = exports.predefinedRoles = {
 }
 
 var permissions = exports.permissions = [
-  {id: 'CreateRole'},
-  {id: 'EditRoleMembers'},
+  {id: 'EditRole'},
+  {id: 'EditCompany'},
+  {id: 'ManageTokens'},
   {id: 'APEditWorkspaces', app: 'AdPeek'},
   {id: 'APEditFolders', app: 'AdPeek'},
   {id: 'APEditCampaigns', app: 'AdPeek'},
@@ -46,22 +47,27 @@ exports.seed = function (knex, Promise) {
     },
     function () {
       return knex('permission_name').insert([
-        {id: uuid(), permission: 'CreateRole', locale: 'pt-BR', name: 'Criar grupo'},
-        {id: uuid(), permission: 'CreateRole', locale: 'en', name: 'Create role'},
-        {id: uuid(), permission: 'EditRoleMembers', locale: 'pt-BR', name: 'Editar membros de grupo'},
-        {id: uuid(), permission: 'EditRoleMembers', locale: 'en', name: 'Edit role members'},
+        {id: uuid(), permission: 'EditRole', locale: 'pt-BR', name: 'Editar grupo'},
+        {id: uuid(), permission: 'EditRole', locale: 'en', name: 'Edit role'},
+
+        {id: uuid(), permission: 'EditCompany', locale: 'pt-BR', name: 'Editar empresa'},
+        {id: uuid(), permission: 'EditCompany', locale: 'en', name: 'Edit company'},
+
+        {id: uuid(), permission: 'ManageTokens', locale: 'pt-BR', name: 'Gerenciar tokens'},
+        {id: uuid(), permission: 'ManageTokens', locale: 'en', name: 'Manage tokens'},
+
         {id: uuid(), permission: 'APEditWorkspaces', locale: 'pt-BR', name: 'Editar Áreas de Trabalho'},
         {id: uuid(), permission: 'APEditWorkspaces', locale: 'en', name: 'Edit Workspaces'},
         {id: uuid(), permission: 'APEditFolders', locale: 'pt-BR', name: 'Editar Pastas'},
         {id: uuid(), permission: 'APEditFolders', locale: 'en', name: 'Edit Folders'},
-        {id : uuid(), permission: 'APEditCampaigns', locale: 'pt-BR', name: 'Editar Campanhas'},
-        {id : uuid(), permission: 'APEditCampaigns', locale: 'en', name: 'Edit Campaigns'},
-        {id : uuid(), permission: 'APEditOrders', locale: 'pt-BR', name: 'Editar Pedidos'},
-        {id : uuid(), permission: 'APEditOrders', locale: 'en', name: 'Edit Orders'},
-        {id : uuid(), permission: 'APBrowseReports', locale: 'pt-BR', name: 'Navegar Relatórios'},
-        {id : uuid(), permission: 'APBrowseReports', locale: 'en', name: 'Browse Reports'},
-        {id : uuid(), permission: 'APEditReports', locale: 'pt-BR', name: 'Editar Relatórios'},
-        {id : uuid(), permission: 'APEditReports', locale: 'en', name: 'Edit Reports'}
+        {id: uuid(), permission: 'APEditCampaigns', locale: 'pt-BR', name: 'Editar Campanhas'},
+        {id: uuid(), permission: 'APEditCampaigns', locale: 'en', name: 'Edit Campaigns'},
+        {id: uuid(), permission: 'APEditOrders', locale: 'pt-BR', name: 'Editar Pedidos'},
+        {id: uuid(), permission: 'APEditOrders', locale: 'en', name: 'Edit Orders'},
+        {id: uuid(), permission: 'APBrowseReports', locale: 'pt-BR', name: 'Navegar Relatórios'},
+        {id: uuid(), permission: 'APBrowseReports', locale: 'en', name: 'Browse Reports'},
+        {id: uuid(), permission: 'APEditReports', locale: 'pt-BR', name: 'Editar Relatórios'},
+        {id: uuid(), permission: 'APEditReports', locale: 'en', name: 'Edit Reports'}
       ])
     },
     function () {
@@ -84,8 +90,9 @@ exports.seed = function (knex, Promise) {
     },
     function () {
       return knex('predefined_role_permission').insert([
-        {id: uuid(), predefined_role: predefinedRoles.admin, permission: 'CreateRole'},
-        {id: uuid(), predefined_role: predefinedRoles.admin, permission: 'EditRoleMembers'}
+        {id: uuid(), predefined_role: predefinedRoles.admin, permission: 'EditRole'},
+        {id: uuid(), predefined_role: predefinedRoles.admin, permission: 'EditCompany'},
+        {id: uuid(), predefined_role: predefinedRoles.admin, permission: 'ManageTokens'}
       ])
     }
   ])
