4445ef01b68c9180426b3059e87fe29c77cd6605
Luan Prestes <luan.prestes@tetris.co>
2021-09-27 15:07:20 -0300

    add migrate

---
+++ migrations/20210927175926_slug-company.js
@@ -0,0 +1,11 @@
+exports.up = function (knex, Promise) {
+return knex.schema.table('company', function (table) {
+        table.string('slug', 24)
+    })
+}
+
+exports.down = function (knex, Promise) {
+    return knex.schema.table('company', function (table) {
+        table.dropColumn('slug')
+    })
+}
