d39a68f0540776d5f0fd92f623614714a78bbbf0
Luan Prestes <luan.prestes@tetris.co>
2021-09-29 14:00:16 -0300

    removendo unique

--- migrations/20210929134200_unique_slug_company.js
+++
@@ -1,9 +0,0 @@
-exports.up = function (knex, Promise) {
-    return knex.schema.alterTable('company', function(t) {
-        t.unique('slug')
-    })
-}
-
-exports.down = function (knex, Promise) {
-
-}
